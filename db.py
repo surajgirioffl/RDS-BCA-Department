013926ed839187d611d0c11faaeee27d3b19a6e6
surajgirioffl <surajkumargiri2002@gmail.com>
2022-10-17 19:22:07 +0530

    Add: Add db module to deal with database
    Add db module to deal with database operations.
    
    Signed-off-by: @surajgirioffl

---
+++ db.py
@@ -0,0 +1,9 @@
+"""
+    @author: Suraj Kumar Giri 
+    @date: 17th Oct 2022
+    @description:
+        * Module to handle database operations.
+"""
+
+__author__ = 'Suraj Kumar Giri'
+__email__ = 'surajgirioffl@gmail.com'
