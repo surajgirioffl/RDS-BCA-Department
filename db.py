ceea2dd82a27a3f2397764f9a37bb9bbe6a7da1e
surajgirioffl <surajkumargiri2002@gmail.com>
2022-10-18 07:57:41 +0530

    Docs: Add docstring of module, classes, methods
    Add docstring of module, classes and methods.
    
    Signed-off-by: @surajgirioffl

--- db.py
+++ db.py
@@ -3,6 +3,10 @@
     @date: 17th Oct 2022
     @description:
         * Module to handle database operations.
+    @classes:
+        * Result:
+            - Class to handle database operations for student's result.
+            - To fetch result of students using various methods.
 """
 
 __author__ = 'Suraj Kumar Giri'
@@ -11,21 +15,57 @@ __email__ = 'surajgirioffl@gmail.com'
 import sqlite3 as sqlite
 
 
-class StudentDetails:
-    ...
-
-
 class Result:
+    """
+        @description:
+            - Class to handle database operations for student's result.
+
+        @methods:
+            * getResult():
+                - To fetch result of students using various methods (using registrationNo, examRoll, classRoll).
+    """
+
     def __init__(self, session: str, semester: int) -> None:
+        """
+            Description:
+                - To initialize the database connection and cursor using student's session and semester.
+
+            Args:
+                * session (str):
+                    - Student's session. E.g., 2020-23, 2021-24, etc.
+                * semester (int):
+                    - Student's semester. E.g., 1,2,5 etc. (Between 1 to 6)
+        """
         self.session = session
         self.semester = semester
         self.conn = sqlite.connect(f"database/{session}.db")
         self.cursor = self.conn.cursor()
 
     def __del__(self):
+        """
+            Description:
+                - To close the database connection when constructor called.
+        """
         self.conn.close()
 
     def __fetchAllData(self, registrationNo: str) -> dict | None:
+        """
+            Description:
+                - To fetch all result related data only using registrationNo.
+
+            Args:
+                * registrationNo (str):
+                    - RegistrationNo of student.
+
+            Returns:
+                * dict:
+                        - If data found on given constraints.
+                        - Dictionary containing all desired values with respective keys. 
+                * None:
+                        - In case of any failure.
+                        - If data not found in database.
+                        - In case of invalid credentials
+        """
         attributes = ('Name', 'examRoll', 'classRoll',
                       'TotalMarks', 'ResultStatus')
         data = self.cursor.execute(
@@ -40,6 +80,30 @@ class Result:
             return requiredData
 
     def getResult(self, registrationNo: str = None, examRoll: int = None, classRoll: int = None) -> dict | None:
+        """
+            Description:
+                - To fetch result of students using different parameters (registrationNo, examRoll, classRoll).
+
+            Args:
+                * registrationNo (str, optional):
+                    - For registration number of student.
+                    - Defaults to None.
+                * examRoll (int, optional):
+                    - For exam roll number of student.
+                    - Defaults to None.
+                * classRoll (int, optional):
+                    - For class roll number of student.
+                    - Defaults to None.
+
+            Returns:
+                * dict:
+                    - If data found on given constraints.
+                    - Dictionary containing all desired values with respective keys. 
+                * None:
+                    - In case of any failure.
+                    - If data not found in database.
+                    - In case of invalid credentials
+        """
         if registrationNo:
             return self.__fetchAllData(registrationNo)
 
@@ -57,6 +121,10 @@ class Result:
             if registrationNo:
                 return self.__fetchAllData(registrationNo)
         return None
+
+
+class StudentDetails:
+    ...
 
 
 if __name__ == '__main__':
