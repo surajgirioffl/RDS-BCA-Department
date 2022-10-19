cf7c60bb7435932c071b6a583780931cec403e5e
surajgirioffl <surajkumargiri2002@gmail.com>
2022-10-20 03:12:52 +0530

    Feat: Add page `result` to handle related operations
    Add page 'result' to handle result related operations.
    
    Signed-off-by: @surajgirioffl

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 import db
 
 app = Flask(__name__)
@@ -6,9 +6,22 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def home():
-    if request.method == "POST":
-        ...
     return render_template('index.html')
+
+
+@app.route('/result', methods=['POST', 'GET'])
+def result():
+    if request.method == "POST":
+        clientDataDict = request.form
+        print(clientDataDict)
+        result = db.Result(clientDataDict.get('session'), clientDataDict.get(
+            'semester'))  # creating instance of Result class
+
+        parameterDict = {clientDataDict.get(
+            'idName'): clientDataDict.get('idValue')}
+        databaseResponse = result.getResult(**parameterDict)
+        return render_template('result.html', result=databaseResponse, isSubmitClicked=True)
+    return render_template('result.html')
 
 
 if __name__ == '__main__':
