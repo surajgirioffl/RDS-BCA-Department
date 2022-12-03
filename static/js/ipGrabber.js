b2912316fd4859f2a151aee637f926c4682e9139
surajgirioffl <surajkumargiri2002@gmail.com>
2022-12-04 03:35:16 +0530

    Update: Update APIs responsible for gathering IP
    Update APIs responsible for gathering IP and add https based API.
    
    Before commit:
    - http based api and browser refuse to send request because server is
      https based and api is http based.
    
    After commit:
    - APIs are HTTPS based
    
    Signed-off-by: @surajgirioffl

--- static/js/ipGrabber.js
+++ static/js/ipGrabber.js
@@ -1,5 +1,7 @@
 /*'https://api.ipify.org?format=json' - api/url to get ip address only*/
-/* we will use http://ip-api.com/json */
+/* we may not able to use http://ip-api.com/json because it is http based api and request is processed if server is https based. */
+/*we fetch user ip using api.ipify.org and then use ipinfo.io to get user location details*/
+/*Now we are using, ipinfo.io which is https based api to get ip details. */
 /*we will do all these things Asynchronously (parallel)*/
 /*we will use XMLHttpRequest() to send request to server*/
 
@@ -8,35 +10,45 @@
 if (window.sessionStorage.getItem("isRequestSent") == null) {
     /*Executing script after page is loaded (No meaning here but I don't want to hamper normal page loading.)*/
     window.onload = function () {
-        const request = new XMLHttpRequest();
-        request.open('GET', 'http://ip-api.com/json', true);/*Asynchronous request*/
-        request.send();
-        request.onload = function () {
-            const response = JSON.parse(this.response);
-            console.log(response);
-            if (request.status == 200) {
-                sendToServer(response);
-            }
-        }
+        /*To get client ip only. Returns client ip on success else false on failure*/
+        const ipRequest = new XMLHttpRequest();
+        ipRequest.open('GET', 'https://api.ipify.org/?format=json', true);/*Asynchronously*/
+        ipRequest.send();
+        ipRequest.onload = () => {
+            if (ipRequest.status == 200) {
+                const clientIP = JSON.parse(ipRequest.response).ip;
 
-        /*function to send data to server*/
-        function sendToServer(response) {
-            const request = new XMLHttpRequest();
-            request.open('POST', 'http://127.0.0.1:5000/ip', true);
-            request.setRequestHeader('Content-Type', 'application/json');
-
-            /*extracting data from response*/
-            const dataToSend = {
-                ip: response.query,
-                city: response.city,
-                pin: response.zip,
-                state: response.regionName,
-                country: response.country,
-                isp: response.isp,
-                timeZone: response.timezone
+                /*another request to get client location and more details based on ip*/
+                const request = new XMLHttpRequest();
+                request.open('GET', `https://ipinfo.io/${clientIP}?token=1a89dcc3cf357d`, true);/*Asynchronous request*/
+                request.send();
+                request.onload = function () {
+                    const response = JSON.parse(this.response);
+                    if (request.status == 200) {
+                        sendToServer(response);
+                    }
+                }
             }
-            request.send(JSON.stringify(dataToSend));
-            window.sessionStorage.setItem("isRequestSent", true);
+
+            /*function to send data to server*/
+            function sendToServer(response) {
+                const request = new XMLHttpRequest();
+                request.open('POST', 'http://127.0.0.1:5000/ip', true);
+                request.setRequestHeader('Content-Type', 'application/json');
+
+                /*extracting data from response*/
+                const dataToSend = {
+                    ip: response.ip,
+                    city: response.city,
+                    pin: response.postal,
+                    state: response.region,
+                    country: response.country,
+                    isp: response.org,
+                    timeZone: response.timezone
+                }
+                request.send(JSON.stringify(dataToSend));
+                window.sessionStorage.setItem("isRequestSent", true);
+            }
         }
     }
 }
