6484f49951bbe9d134dac2f79253ab38167bbc88
surajgirioffl <surajkumargiri2002@gmail.com>
2022-12-05 13:22:08 +0530

    Update: Set the timezone for UNIX system only
    Set the timezone for UNIX system only because time.tzset() function
    works on UNIX only.
    
    Signed-off-by: @surajgirioffl

--- setTimeZone.py
+++ setTimeZone.py
@@ -2,9 +2,11 @@
 # Source: https://help.pythonanywhere.com/pages/SettingTheTimezone
 import os
 import time
+from sys import platform
 
 
 def setTimeZone(timezone: str = "Asia/Kolkata"):
     os.environ["TZ"] = timezone  # define TZ environment variable
-    time.tzset()  # set the timezone
-    # tzset() will only work if platform is not Windows.
+    if platform != "win32":
+        time.tzset()  # set the timezone
+        # tzset() will only work if platform is not Windows.
