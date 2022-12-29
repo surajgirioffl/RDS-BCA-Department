56827678468a5bc0bb4b9ebf680982ecf88c90e2
surajgirioffl <surajkumargiri2002@gmail.com>
2022-12-30 01:17:36 +0530

    Feat: Add feat to get credentails based on username
    Add feature to get credentails based on username.
    
    Signed-off-by: @surajgirioffl

--- PythonAnyWhere/credentials.py
+++ PythonAnyWhere/credentials.py
@@ -8,12 +8,31 @@
         * Module to store and handle credentials related to account and APIs of PythonAnyWhere.
         
     @global-variables:
-        *
+        * host
+            - Host name of PythonAnyWhere.
 """
 
 __author__ = 'Suraj Kumar Giri'
 __email__ = 'surajgirioffl@gmail.com'
 
-username = 'rdsbca'
-apiToken = '08c8fd81396723df0627ce2a5cd3550d5ddc9a08'
-host = 'https://www.pythonanywhere.com'
+
+host = 'https://www.pythonanywhere.com'  # host name of PythonAnyWhere
+
+
+def getApiToken(username: str = 'rdsbca') -> str:
+    """
+        Description:
+            * Function to get API token for the given username.
+
+        Args:
+            * username: str (default: 'rdsbca')
+                - Username of the account.
+
+        Returns:
+            * str:
+                - API token for the given username.
+    """
+    tokenDictionary = {
+        'rdsbca': '08c8fd81396723df0627ce2a5cd3550d5ddc9a08'
+    }
+    return tokenDictionary[username]
