66a3afd9964b712aabdbc049ff05afbc6bfff706
surajgirioffl <surajkumargiri2002@gmail.com>
2022-12-30 18:29:11 +0530

    Feat: Add feat to fetch multiple details using API
    Add functionality to fetch multiple details using API of PythonAnyWhere.
    
    Signed-off-by: @surajgirioffl

--- PythonAnyWhere/main.py
+++ PythonAnyWhere/main.py
@@ -4,7 +4,7 @@
     @author: Suraj Kumar Giri
     @init-date: 29th Dec 2022
     @last-modified: 30th Dec 2022
-    
+
     @description:
         * Module deals with all required APIs of PythonAnyWhere.
 """
@@ -16,36 +16,79 @@ import api
 import tools
 from time import sleep
 
-choice, username = menu.displayMenu()
-apiEndpoint, requestType = api.getEndpoint(apiId=choice)
-apiToken = getApiToken(username)  # API token
-header = {'Authorization': f'Token {apiToken}'}  # Authorization header
+while True:
+    # Display menu and get user's choice and username
+    choice, username = menu.displayMenu()
 
-variableInEndpoint = tools.getVariablesFromFormat(apiEndpoint)
-print(variableInEndpoint)
+    # Get API endpoint and request type and other required credentials
+    apiEndpoint, requestType = api.getEndpoint(apiId=choice)
+    apiToken = getApiToken(username)  # API token
+    header = {'Authorization': f'Token {apiToken}'}  # Authorization header
 
-apiEndpoint = apiEndpoint.format(username=username)
+    # fetching variables from endpoint
+    variableInEndpoint = tools.getVariablesFromFormat(apiEndpoint)
+    print("Variable in API endpoint: ", variableInEndpoint)
 
-print('Time after which request will made in seconds = 9\033[?25l', end="")
-for i in range(9):
+    # replacing variables in endpoint with desired credentials
+    isMoreThanOneVariable = False
+    if len(variableInEndpoint) == 1:  # means only username is required
+        apiEndpoint = apiEndpoint.format(username=username)
+        break
+    else:
+        variablesValue = {}
+        for variable in variableInEndpoint:
+            if variable == 'username':
+                variablesValue[variable] = username
+            else:
+                isMoreThanOneVariable = True
+                value = input(f"\nEnter {variable} (Press # for menu): ")
+                if value == '#':
+                    continue
+                else:
+                    variablesValue[variable] = value
+        break
+
+# replacing variables in endpoint with desired credentials in case of more than one variable in format string
+if isMoreThanOneVariable:
+    apiEndpoint = apiEndpoint.format(**variablesValue)
+
+# Displaying API endpoint and request type and other details to user for verification
+print(f"""\n
+      Username: {username}
+      Endpoint: {apiEndpoint}
+      Request: {requestType}
+      """)
+
+# Sleep for 5 seconds before making request for user to cancel request  or verify the credentials before making request
+print('Time after which request will made in seconds = 5\033[?25l', end="")
+for i in range(5):
     sleep(1)
-    print(f'\b{9-i}', end="")
+    print(f'\b{5-i-1}', end="")
 
+#----------------- Making request -----------------#
+# source: https://requests.readthedocs.io/en/latest/api/
+response = request.request(method=requestType,
+                           url=f"{host+apiEndpoint}", headers=header)  # we will not use request.get() because request type can be POST, PUT, DELETE etc.
 
-response = request.get(
-    url=f"{host+apiEndpoint}", headers=header)
+print("Status Code:", response.status_code)
+
+print("\n----------------------------- Response --------------------------------")
+print("\nResponse type:", type(response.json()))
+# print(response.json())
+
 if response.status_code == 200:
-    print(type(response.json()))
-    print(response.json())
-
     if isinstance(response.json(), list):
         for json in response.json():
             for key, value in json.items():
                 print(key, ":", value)
             print()
-    else:
+    elif isinstance(response.json(), dict):
         for key, value in response.json().items():
             print(key, ":", value)
+    else:
+        print(response.json())
 else:
-    print("Error:", response.status_code, response.reason)
+    print("\nError:", response.status_code, response.reason)
     print("Response:", response.text)
+
+print("\n")
