4b8cc4a3f80eaea0cddc2fdc22fb33c2d228ffaa
surajgirioffl <surajkumargiri2002@gmail.com>
2023-01-02 23:51:22 +0530

    Update: Update rounds of hashing
    Update rounds of hashing from 8 to 12 (default) to balanced security as
    well as performance.
    
    Signed-off-by: @surajgirioffl

--- app_scripts/crypt.py
+++ app_scripts/crypt.py
@@ -1,7 +1,7 @@
 import bcrypt
 
 
-def encryptPassword(password: str, rounds: int = 8):
+def encryptPassword(password: str, rounds: int = 12):
     # rounds specifies the complexity of the hash. It is an integer that specifies the number of rounds of hashing to perform when generating the salt.
     salt = bcrypt.gensalt(rounds=rounds)
     hash = bcrypt.hashpw(password.encode('utf-8'), salt)
