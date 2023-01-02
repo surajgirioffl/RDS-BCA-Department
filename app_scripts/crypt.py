9bb110130f511cf8af034d25f10242b97f88293f
surajgirioffl <surajkumargiri2002@gmail.com>
2023-01-02 23:49:38 +0530

    Feat: Add feat. to verify password using hash
    Add feature to verify password using hash.
    
    Signed-off-by: @surajgirioffl

--- app_scripts/crypt.py
+++ app_scripts/crypt.py
@@ -7,3 +7,14 @@ def encryptPassword(password: str, rounds: int = 8):
     hash = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hash
 
+
+def checkPassword(password: str, hash: str) -> bool:
+    if not isinstance(hash, bytes):
+        hash = hash.encode('utf-8')
+    return bcrypt.checkpw(password.encode('utf-8'), hash)
+
+
+if __name__ == "__main__":
+    hash = encryptPassword('Suraj Giri')
+    print(hash)
+    print(checkPassword("Suraj Giri", hash))
