14d34b7a6635badde212225d25344072e484dfb0
surajgirioffl <surajkumargiri2002@gmail.com>
2023-01-10 00:37:40 +0530

    Update: Update data types of some attributes
    Update data types of some attributes from CHAR to VARCHAR.
    
    Signed-off-by: @surajgirioffl

--- schema/session-wise-database.sql
+++ schema/session-wise-database.sql
@@ -3,18 +3,18 @@
  * Schema for database of different sessions like `2020-23`, `2021-24`, ... or `rdsbca$2020-23', `rdsbca$2021-24`, `rdsbca$2022-25`, ...
  * Created by: Suraj Kumar Giri
  * Created on: 9th Jan 2023
- * Last updated on: 9th Jan 2023
+ * Last updated on: 10th Jan 2023
  */
 ----
 -- 1. Table `Students` 
 ----
 CREATE TABLE `Students` (
     `SNo` TINYINT UNSIGNED NOT NULL UNIQUE AUTO_INCREMENT,
-    `RegistrationNo` CHAR(50) PRIMARY KEY NOT NULL,
+    `RegistrationNo` VARCHAR(50) PRIMARY KEY NOT NULL,
     `ExamRoll` MEDIUMINT UNIQUE NOT NULL,
     `ClassRoll` TINYINT UNIQUE NOT NULL,
-    `Name` CHAR(50) NOT NULL,
-    `Gender` CHAR(10) NOT NULL DEFAULT 'Male',
+    `Name` VARCHAR(50) NOT NULL,
+    `Gender` VARCHAR(10) NOT NULL DEFAULT 'Male',
 );
 
 -- First execute above query then run below query because of foreign key constraint.
@@ -23,10 +23,10 @@ CREATE TABLE `Students` (
 ----
 CREATE TABLE `Contact` (
     `SNo` TINYINT UNSIGNED NOT NULL UNIQUE AUTO_INCREMENT,
-    `RegistrationNo` CHAR(50) PRIMARY KEY NOT NULL,
-    `Email` CHAR(100) UNIQUE NOT NULL,
-    `PhoneNo` CHAR(15) UNIQUE,
-    `City` CHAR(30),
+    `RegistrationNo` VARCHAR(50) PRIMARY KEY NOT NULL,
+    `Email` VARCHAR(100) UNIQUE NOT NULL,
+    `PhoneNo` VARCHAR(15) UNIQUE,
+    `City` VARCHAR(30),
     FOREIGN KEY(RegistrationNo) REFERENCES Students(RegistrationNo)
 );
 
@@ -36,7 +36,7 @@ CREATE TABLE `Contact` (
 ----
 CREATE TABLE `ResultSem1`(
     `SNo` TINYINT UNSIGNED NOT NULL UNIQUE AUTO_INCREMENT,
-    `RegistrationNo` CHAR(50) PRIMARY KEY NOT NULL,
+    `RegistrationNo` VARCHAR(50) PRIMARY KEY NOT NULL,
     `ExamRoll` MEDIUMINT UNIQUE NOT NULL,
     `TotalMarks` SMALLINT NOT NULL,
     `ResultStatus` VARCHAR(100) NOT NULL DEFAULT 'Pass',
