e72da9147d73a41e3f5b1f91f1ca187669152b80
surajgirioffl <surajkumargiri2002@gmail.com>
2023-01-10 23:43:40 +0530

    Add: Add schema for a new table `visitors`
    Add schema for a new table named as 'visitors'.
    
    Signed-off-by: @surajgiriofll

--- schema/visitors.sql
+++ schema/visitors.sql
@@ -5,3 +5,12 @@
  * Created on: 10th Jan 2023
  * Last Updated on: 10th Jan 2023
  */
+----
+-- 1. Table `visitors`
+----
+CREATE TABLE IF NOT EXISTS `visitors`(
+    `Id` MEDIUMINT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `Ip` VARCHAR(20) NOT NULL UNIQUE,
+    `LastVisited` DATETIME NOT NULL,
+    `VisitCount` SMALLINT UNSIGNED DEFAULT 0
+);
