6aae10959fcd5745435adea728500ab12a8c4a44
surajgirioffl <surajkumargiri2002@gmail.com>
2023-01-11 03:24:34 +0530

    Add: Add one more attribute `Username` in a table
    Add one more attribute 'Username' in the 'visitors' table.
    
    Signed-off-by: @surajgirioffl

--- schema/visitors.sql
+++ schema/visitors.sql
@@ -3,7 +3,7 @@
  * Schema for database `visitors` or `rdsbca$visitors`
  * Created by: Suraj Kumar Giri
  * Created on: 10th Jan 2023
- * Last Updated on: 10th Jan 2023
+ * Last Updated on: 11th Jan 2023
  */
 ----
 -- 1. Table `visitors`
@@ -11,6 +11,7 @@
 CREATE TABLE IF NOT EXISTS `visitors`(
     `Id` MEDIUMINT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
     `Ip` VARCHAR(20) NOT NULL UNIQUE,
+    `Username` VARCHAR(30) DEFAULT NULL,
     `LastVisited` DATETIME NOT NULL,
     `VisitCount` SMALLINT UNSIGNED DEFAULT 0
 );
