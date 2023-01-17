1de9374b62ca8d762d4cd8c637352e816eab889c
surajgirioffl <surajkumargiri2002@gmail.com>
2023-01-17 22:22:03 +0530

    Add: Add schema of a new table `notice`
    Add schema of a new table 'notice'.
    
    Signed-off-by: @surajgirioffl

--- schema/dynamic-content.sql
+++ schema/dynamic-content.sql
@@ -2,4 +2,23 @@
 -- Schema for database `dynamic_contents` or `rdsbca$dynamic_contents`
 -- Created by: Suraj Kumar Giri
 -- Created on: 16th Jan 2023
--- Last updated on: 16th Jan 2023
+-- Last updated on: 17th Jan 2023
+----
+-- Table `notice`
+----
+CREATE TABLE IF NOT EXISTS `notice` (
+    `SNo` SMALLINT UNSIGNED NOT NULL UNIQUE AUTO_INCREMENT,
+    `TopMarquee` VARCHAR(300) NOT NULL,
+    `TopMarqueeLink` VARCHAR(100) NOT NULL,
+    `Title` VARCHAR(300) NOT NULL,
+    `DocsDownloadTitle` VARCHAR(200) NOT NULL,
+    `DocsDownloadLink` VARCHAR(100) DEFAULT NULL,
+    `ButtonTitle` VARCHAR(200) DEFAULT NULL,
+    `ButtonLink` VARCHAR(100) DEFAULT NULL,
+    `BlinkerTitle` VARCHAR(80) NOT NULL,
+    `BlinkingText` VARCHAR(50) NOT NULL,
+    `BottomMarquee1` VARCHAR(300) NOT NULL,
+    `BottomMarquee2` VARCHAR(300) DEFAULT NULL,
+    `BottomMarquee3` VARCHAR(300) DEFAULT NULL,
+    `DateModified` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+);
