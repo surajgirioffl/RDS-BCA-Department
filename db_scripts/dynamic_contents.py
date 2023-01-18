8e81d4a0fbe9e98428cf1d2cbe861943dfb963e3
surajgirioffl <surajkumargiri2002@gmail.com>
2023-01-18 20:16:32 +0530

    Add: Add module to serve dynamic contents
    Add module to serve dynamic contents on the website.
    
    Signed-off-by: @surajgirioffl

---
+++ db_scripts/dynamic_contents.py
@@ -0,0 +1,9 @@
+"""
+    @file: dynamic_contents.py
+    @author: Suraj Kumar Giri
+    @init-date: 18th Jan 2023
+    @last-modified: 18th Jan 2023
+    
+    @description:
+        * Module to handle database related operations related to serving dynamic contents on the website.
+"""
