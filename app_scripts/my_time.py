452b1a666a0688d20585949c0004bec5f96edde0
surajgirioffl <surajkumargiri2002@gmail.com>
2023-01-19 23:57:09 +0530

    Feat: Add feat to convert MySQL datetime to readable
    Add feature to convert the MySQL datetime string to redable format to be
    used in the web app.
    
    Signed-off-by: @surajgirioffl

--- app_scripts/my_time.py
+++ app_scripts/my_time.py
@@ -31,5 +31,36 @@ def readableDateTime(dateTime: datetime.datetime = datetime.datetime.now()) -> s
     return dateTime.strftime("%d %b %Y %I:%M:%S %p")
 
 
+def readableMySqlDateTime(dateTime: str) -> str:
+    """
+        Description:
+            - Function to convert MySQL datetime string to readable format.
+            - Readable format is like 19 Jan 2023 12:30:00 PM
+
+        Args:
+            * dateTime (str):
+                - MySQL datetime string to be converted in to readable format.
+
+        Returns:
+            * str:
+                - Readable datetime string.
+                - Format is like 19 Jan 2023 12:30:00 PM
+    """
+    # Below is the format of MySQL datetime string
+    # 2023-01-09 13:55:30
+
+    # fetching each element from the datetime string
+    dateTimeDict = {
+        'year': int(dateTime[:4]),
+        'month': int(dateTime[5:7]),
+        'day': int(dateTime[8:10]),
+        'hour': int(dateTime[11:13]),
+        'minute': int(dateTime[14:16]),
+        'second': int(dateTime[17:])
+    }
+    return readableDateTime(datetime.datetime(**dateTimeDict))
+
+
 if __name__ == '__main__':
     print(readableDateTime())
+    print(readableMySqlDateTime('2023-01-09 13:55:30'))
