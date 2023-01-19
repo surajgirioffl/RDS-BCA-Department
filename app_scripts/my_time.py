a5c7010fcfc9fc922e3ee1a1342cd361f59769d8
surajgirioffl <surajkumargiri2002@gmail.com>
2023-01-19 23:48:04 +0530

    Initial: Add module `my_time`
    Add module 'my_time'.
    
    Signed-off-by: @surajgirioffl

---
+++ app_scripts/my_time.py
@@ -0,0 +1,11 @@
+"""
+    @author: Suraj Kumar Giri
+    @init-date: 19th Jan 2023
+    @last-modified: 19th Jan 2023
+
+    @description:
+        * Module to handle all the time related operations involving in the web app.
+    
+    @functions:
+        *
+"""
