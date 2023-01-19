81a477f493b33bca7d5faadfaa4084174fbb06dd
surajgirioffl <surajkumargiri2002@gmail.com>
2023-01-19 21:41:43 +0530

    Add: Add new module in the package
    Add new module in the package.
    
    Signed-off-by: @suarajgirioffl

--- db_scripts/__init__.py
+++ db_scripts/__init__.py
@@ -2,5 +2,6 @@ from . import previous_year_questions_db
 from . import results_db
 from . import userIPDb
 from . import registration_db
+from . import dynamic_contents
 
 __author__ = 'Suraj Kumar Giri'
