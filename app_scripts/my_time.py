92164072d3183c8925e603dc63028c04edad3d13
surajgirioffl <surajkumargiri2002@gmail.com>
2023-01-19 23:53:03 +0530

    Feat: Add feat. to convert date-time to readable
    Add feature to convert the date-time to readable format to be used in
    the web app.
    
    Signed-off-by: @surajgirioffl

--- app_scripts/my_time.py
+++ app_scripts/my_time.py
@@ -9,3 +9,27 @@
     @functions:
         *
 """
+import datetime
+
+
+def readableDateTime(dateTime: datetime.datetime = datetime.datetime.now()) -> str:
+    """
+        Description:
+            - Function to convert datetime object to readable format.
+            - Readable format is like 19 Jan 2023 12:30:00 PM
+
+        Args:
+            * dateTime (datetime.datetime, optional):
+                - Datetime object to be converted to readable format.
+                - Default to current datetime.
+
+        Returns:
+            * str:
+                - Readable datetime string.
+                - Format is like 19 Jan 2023 12:30:00 PM
+    """
+    return dateTime.strftime("%d %b %Y %I:%M:%S %p")
+
+
+if __name__ == '__main__':
+    print(readableDateTime())
