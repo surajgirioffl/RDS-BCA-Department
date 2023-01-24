943f8f39bf82bcd917641de8ec8895df1ddb56e3
surajgirioffl <surajkumargiri2002@gmail.com>
2023-01-24 20:45:21 +0530

    Init: Add a new module `files_db`
    Add a module 'files_db' to insert data in the database rdsbca$files.
    
    Signed-off-by: @surajgirioffl

---
+++ automation_scripts/files_db.py
@@ -0,0 +1,9 @@
+"""
+    @file: files_db.py
+    @author: Suraj Kumar Giri
+    @init-date: 24th Jan 2023
+    @last-modified: 24th Jan 2023
+    
+    @description:
+        * Module to insert data into the database rdsbca$files.
+"""
