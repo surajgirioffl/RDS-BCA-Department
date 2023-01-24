b292471ee6eac1c0fe8694c94c283f3e8ff191eb
surajgirioffl <surajkumargiri2002@gmail.com>
2023-01-25 02:54:18 +0530

    Add: Add class `Files`
    Add class 'Files' to perform insertion operation related to the database
    rdsbca$files.
    
    Also, add feat. to connect with the desired database.
    
    Signed-off-by: @surajgirioffl

--- automation_scripts/files_db.py
+++ automation_scripts/files_db.py
@@ -7,3 +7,40 @@
     @description:
         * Module to insert data into the database rdsbca$files.
 """
+from os import environ
+import mysql.connector as mysql
+
+
+class Files:
+    def __init__(self, host: str = environ.get('DBHOST'), user: str = environ.get('DBUSERNAME'), port: int = int(environ.get('DBPORT')) if environ.get('DBPORT') is not None else 3306, password: str = environ.get('DBPASSWORD'), database: str = "rdsbca$files", timeZoneForDatabase="Asia/Kolkata") -> None:
+        try:
+            # establishing connection and creating cursor object
+            self.conn = mysql.connect(
+                host=f'{host}', user=f'{user}', password=f'{password}', port=port)
+            self.cursor = self.conn.cursor(buffered=True)
+
+            # using database as per the parameter
+            self.cursor.execute(f'USE {database}')
+        except Exception as e:
+            print(
+                f"Unable to establish connection with the database ({database}). Error code 1300")
+            print("Exception: ", e)
+            # if connection is not established then set connectionStatus to False.
+            self.connectionStatus = False
+        else:
+            # if connection is established then set connectionStatus to True.
+            self.connectionStatus = True
+            print(f"Connection with database established ({database}).")
+
+            # setting the timezone for database
+            try:
+                self.cursor.execute(f"SET time_zone = '{timeZoneForDatabase}'")
+            except Exception as e:
+                print("Unable to set time_zone. Error code 1301")
+
+    def __del__(self):
+        ...
+
+
+if __name__ == '__main__':
+    Files()
