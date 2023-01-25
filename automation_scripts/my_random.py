10cc45fdc4310d5417753599ed1a381f2506764d
surajgirioffl <surajkumargiri2002@gmail.com>
2023-01-25 10:44:36 +0530

    Init: Add module to generate random number
    Add module to generate random number on various basis.
    
    Signed-off-by: @surajgirioffl

---
+++ automation_scripts/my_random.py
@@ -0,0 +1,10 @@
+"""
+    @file: my_random.py
+    @author: Suraj Kumar Giri
+    @init-date: 25th Jan 2023
+    @last-modified: 25th Jan 2023
+    
+    @description:
+        * Module to generate random numbers of desired digits.
+        * Also check if number already exists in the database or not.
+"""
