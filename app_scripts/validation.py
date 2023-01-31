58652275518372806a09963cbb70f9e0529dded7
surajgirioffl <surajkumargiri2002@gmail.com>
2023-02-01 00:10:31 +0530

    Add: Add a new module `validation'
    Add a new module 'validation.py' in the package 'app_scripts'.
    
    Signed-off-by: @surajgirioffl

---
+++ app_scripts/validation.py
@@ -0,0 +1,9 @@
+"""
+    @file: validation.py
+    @author: Suraj Kumar Giri
+    @init-date: 1st Feb 2023
+    @last-modified: 1st Feb 2023
+    
+    @description:
+        * Module to validate the data/input sent from client to server as per application configuration and requirements.
+"""
