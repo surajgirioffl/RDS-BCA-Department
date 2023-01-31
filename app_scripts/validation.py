a96d8334e0e7cd4adf626fab67b90cc32ad15460
surajgirioffl <surajkumargiri2002@gmail.com>
2023-02-01 01:55:22 +0530

    Feat: Add feat. to check validation of session
    Add feature to check validation of session as per application's
    specification.
    
    Signed-off-by: @surajgirioffl

--- app_scripts/validation.py
+++ app_scripts/validation.py
@@ -3,7 +3,7 @@
     @author: Suraj Kumar Giri
     @init-date: 1st Feb 2023
     @last-modified: 1st Feb 2023
-    
+
     @description:
         * Module to validate the data/input sent from client to server as per application configuration and requirements.
         * Warning:
@@ -19,3 +19,46 @@
         * Usage:
             - May be used as first server side validation check for data received from the client.
 """
+
+
+def isValidSession(session: str) -> bool:
+    """
+        Description:
+            - Function to check if the session is valid
+            - As per the specification of the application:
+                - valid session is like: 2015-18, 2020-23, 2019-22 and so on.
+
+        Args:
+            * session (str):
+                - session to check.
+
+        Returns:
+            * bool:
+                - True if the session is valid else False.
+    """
+    ##### Conditions for session as per application's specification #####
+    # session contains one hyphen(-) and rest characters must be digits.
+    # year in session must be after 2000 means 2001, 2020, 2010 and so on... (means must be starts with '2')
+    # difference between starting year (before hyphen(-)) and ending year (after hyphen(-)) must be 3.
+    # length of session must be 7.
+
+    # in case of None or empty string passed as session.
+    if session in [None, ""]:
+        return False
+
+    # validating number of characters in session
+    if len(session) == 7:
+        # checking session must contains 1 hyphen and rest characters must be digits and session must be startswith '2'
+        # checking difference between 2 digits before hyphen and 2 digits after hyphen
+        if False if session.count('-') == 1 and session.replace('-', '').isdigit() and session.startswith('2') and eval('{starts}-{ends}'.format(starts=session[:4], ends=session[:2]+session[5:])) == -3 else True:
+            return False
+        return True  # if all conditions are satisfied
+    else:
+        return False
+
+
+if __name__ == "__main__":
+    data = input("Enter data: ")
+    if data == "":
+        data = None
+    print(isValidSession(data))
