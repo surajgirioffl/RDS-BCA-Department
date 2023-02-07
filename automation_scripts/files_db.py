4fe33bcf4995900bb76fab382c6dc8834fe619b3
surajgirioffl <surajkumargiri2002@gmail.com>
2023-02-07 11:13:30 +0530

    Remove: Remove code written for a particular table
    Remove code written for only a particular table.
    
    Signed-off-by: @surajgirioffl

--- automation_scripts/files_db.py
+++ automation_scripts/files_db.py
@@ -86,81 +86,11 @@ class Files:
         self.passDefault = ["SNo", "Access", "ServeVia", "DateCreated",
                             "DateModified", "SubmittedOn", "UploadedOn", "LastModifiedOn", "ApprovedOn"]
 
-        # don't ask from 
-    # Now taking input from user and inserting into database (Table by Table).
     def files(self) -> int:
-        print("------------------------------Table files------------------------------")
-        while True:
-            # for attribute 'Title'
-            while True:
-                title = input(
-                    "Write title of file (It will display on the web page): ")
-                if title == "":
-                    print("Title can't be empty. Please write again...")
-                else:
-                    title = title.title()  # converting into title case
-                    break
-
-            # for attribute 'Access'
-            while True:
-                print("""
-                        Select the access right:
-                        1. Public (Default. Press enter)
-                        2. Private
-                        3. Restricted
-                    """)
-                choice = input("write your choice: ")
-                if choice == "1" or choice == "":
-                    access = "Public"
-                    break
-                elif choice == "2":
-                    access = "Private"
-                    break
-                elif choice == "3":
-                    access = "Restricted"
-                    break
-                else:
-                    print("Invalid choice... Please select again")
-
-            # for attribute 'ServeVia'
-            while True:
-                print("""
-                        Select the way to serve the file to client:
-                        1. Drive (Default. Press enter)
-                        2. FileSystem
-                    """)
-                choice = input("write your choice: ")
-                if choice == "1" or choice == "":
-                    serveVia = "Drive"
-                    break
-                elif choice == "2":
-                    serveVia = "FileSystem"
-                    break
-                else:
-                    print("Invalid choice... Please select again")
-
-            # verification of input data
-            print("================VERIFICATION=================")
-            print(f"""
-                    For table 'files':
-                    1. Title: {title}
-                    2. Access: {access}
-                    3. ServeVia: {serveVia}
-                    Press enter or yes to continue else press any key to re-enter the information.
-                """)
-            choice = input("write your choice: ")
-            if choice in ["", "yes", "Yes", "YES"]:
-                break
-
-        fileId = myRandom.Random(self.cursor, 'files', 'FileId').generate()
-
-        # Inserting data in table
-        self.cursor.execute(f"""-- sql
-                                INSERT INTO {self.tables.get('files')}
-                                VALUES (
-                                        DEFAULT, {fileId}, '{title}', '{access}', '{serveVia}'
-                                    );
-                            """)
+        """
+            Now taking input from user and inserting into database (Table by Table).
+        """
+        ...
 
 
 def main():
