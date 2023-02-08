a7ffbde8db56c1237fdeb71c55b5ff5b75287248
surajgirioffl <surajkumargiri2002@gmail.com>
2023-02-08 20:39:37 +0530

    Update: Update datatype of an attribute
    Update datatype of an attribute.
    
    Signed-off-by: @surajgirioffl

--- schema/session-wise-database.sql
+++ schema/session-wise-database.sql
@@ -14,7 +14,7 @@ CREATE TABLE IF NOT EXISTS `students` (
     `ExamRoll` MEDIUMINT UNIQUE NOT NULL,
     `ClassRoll` TINYINT UNIQUE NOT NULL,
     `Name` VARCHAR(50) NOT NULL,
-    `Gender` VARCHAR(10) NOT NULL DEFAULT 'Male'
+    `Gender` ENUM('Male', 'Female', 'Others') NOT NULL DEFAULT 'Male'
 );
 
 -- First execute above query then run below query because of foreign key constraint.
