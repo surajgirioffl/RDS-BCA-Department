8324e93ae603fed0981ea731745ce832afe1acc3
surajgirioffl <surajkumargiri2002@gmail.com>
2023-02-17 19:09:04 +0530

    Add: Add a script for operations related to bca db
    Add a new script responsible for operations related to the rdsbca$bca
    database.
    
    Signed-off-by: @surajgirioffl

---
+++ db_scripts/bca_db.py
@@ -0,0 +1,12 @@
+"""
+    @author: Suraj Kumar Giri
+    @init-date: 17th Oct 2022
+    @last-modified: 17th Feb 2023
+    @error-series: 1600
+    @description:
+        * Module to handle database operations related with the database bca.
+    @classes:
+"""
+
+__author__ = 'Suraj Kumar Giri'
+__email__ = 'surajgirioffl@gmail.com'
