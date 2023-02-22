45d08666171fafc93651e120bb69e5b80201b524
surajgirioffl <surajkumargiri2002@gmail.com>
2023-02-22 12:13:41 +0530

    Feat: Add feat. to switch between images
    Add functionality to switch between images while displaying the image in
    fullscreen.
    
    Signed-off-by: @surajgirioffl

--- static/js/gallery.js
+++ static/js/gallery.js
@@ -5,6 +5,17 @@ function displayFullScreen(image) {
     const fullScreenContainer = document.createElement('div');
     fullScreenContainer.id = 'fullscreen-div';
 
+    /*button to view previous image */
+    const previousImageButton = document.createElement('span');
+    previousImageButton.className = "carousel-control-prev-icon";
+    previousImageButton.addEventListener('click', () => {
+        if (image.previousElementSibling.src != null) {
+            image = image.previousElementSibling;
+            copyImage.src = image.src;
+        }
+    })
+    fullScreenContainer.appendChild(previousImageButton);
+
     /*div containing images*/
     const imageContainer = document.createElement('div');
     imageContainer.id = 'image-container-div';
@@ -15,6 +26,17 @@ function displayFullScreen(image) {
     copyImage.src = image.src;
     imageContainer.appendChild(copyImage);
     console.log('done');
+
+    /*button to view next image */
+    const nextImageButton = document.createElement('span');
+    nextImageButton.className = "carousel-control-next-icon";
+    nextImageButton.addEventListener('click', () => {
+        if (image.nextElementSibling.src != null) {
+            image = image.nextElementSibling;
+            copyImage.src = image.src;
+        }
+    })
+    fullScreenContainer.appendChild(nextImageButton);
 
     /*appending the main fullScreenContainer to the body*/
     document.body.appendChild(fullScreenContainer);
