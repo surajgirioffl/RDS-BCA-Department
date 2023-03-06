403c3825be1a9757c59df61f6d1c8ea31813556f
surajgirioffl <surajkumargiri2002@gmail.com>
2023-03-06 09:09:15 +0530

    Add: Add MySQL schema file for db `access_control`
    Add MySQL schema file for the database 'access_control'.
    
    Signed-off-by: @surajgirioffl

---
+++ schema/access-control.sql
@@ -0,0 +1,7 @@
+/*
+ * MySQL
+ * Schema for database `access_control` or `rdsbca$access_control`.
+ * Created by: Suraj Kumar Giri
+ * Created on: 6th March 2023
+ * Last updated 6th March 2023
+ */
