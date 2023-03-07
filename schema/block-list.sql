340784663a22b74415e41c1fb3ef9b1f9bd87a52
surajgirioffl <surajkumargiri2002@gmail.com>
2023-03-07 13:47:13 +0530

    Add: Add schema for a new table `user_blocklist`
    Add schema for a new table named as 'user_blocklist'.
    
    Signed-off-by: @surajgirioffl

--- schema/block-list.sql
+++ schema/block-list.sql
@@ -3,5 +3,16 @@
  * Schema for database `block_list` or `rdsbca$block_list`.
  * Created by: Suraj Kumar Giri
  * Created on: 6th March 2023
- * Last updated 6th March 2023
+ * Last updated 7th March 2023
  */
+CREATE TABLE IF NOT EXISTS `user_blocklist`(
+    `SNo` SMALLINT UNSIGNED NOT NULL UNIQUE AUTO_INCREMENT,
+    `BlockId` INT UNSIGNED PRIMARY KEY NOT NULL,
+    `UserId` INT UNSIGNED NOT NULL,
+    `BlockedOn` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `BlockedBy` VARCHAR(50) NOT NULL,
+    `BlockReason` VARCHAR(500) NOT NULL,
+    `UnblockedOn` DATETIME DEFAULT NULL,
+    `UnblockedBy` VARCHAR(50) DEFAULT NULL
+    /*BlockedBy & UnblockedBy - username or person/moderator/adminstrator who blocked the user*/
+);
