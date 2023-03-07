46e167fa36a092a6519c327c7b7d722d64db3fbc
surajgirioffl <surajkumargiri2002@gmail.com>
2023-03-07 13:25:48 +0530

    Add: Add schema for a new table `users`
    Add schema for a new table named as 'users'.
    
    Signed-off-by: @surajgirioffl

--- schema/users.sql
+++ schema/users.sql
@@ -3,5 +3,11 @@
  * Schema for database `users` or `rdsbca$users`.
  * Created by: Suraj Kumar Giri
  * Created on: 6th March 2023
- * Last updated 6th March 2023
+ * Last updated 7th March 2023
  */
+CREATE TABLE IF NOT EXISTS `users`(
+    `SNo` SMALLINT UNSIGNED NOT NULL UNIQUE AUTO_INCREMENT,
+    `UserId` INT UNSIGNED PRIMARY KEY NOT NULL,
+    `Username` VARCHAR(50) UNIQUE NOT NULL,
+    `Password` VARCHAR(100) NOT NULL,
+);
