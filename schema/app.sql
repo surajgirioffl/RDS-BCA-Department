1ebde3ae1a37cc16d930e8e04f82269d52120486
surajgirioffl <surajkumargiri2002@gmail.com>
2023-03-07 13:56:45 +0530

    Add: Add schema for a new table `otp_records`
    Add schema for a new table named as 'otp_records'.
    
    Signed-off-by: @surajgirioffl

--- schema/app.sql
+++ schema/app.sql
@@ -5,3 +5,12 @@
  * Created on: 7th March 2023
  * Last updated 7th March 2023
  */
+CREATE TABLE IF NOT EXISTS `otp_records` (
+    `SNo` SMALLINT UNSIGNED NOT NULL UNIQUE AUTO_INCREMENT,
+    `OtpId` INT UNSIGNED PRIMARY KEY NOT NULL,
+    `Email` VARCHAR(100) NOT NULL,
+    /*use 7 digits or less for otp (for MEDIUMINT)*/
+    `Otp` MEDIUMINT UNSIGNED NOT NULL,
+    `GeneratedOn` DATETIME DEFAULT CURRENT_TIMESTAMP,
+    `ExpireOn` DATETIME DEFAULT NULL,
+);
