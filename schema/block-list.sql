15bd70454bb962dabed597c75f3a0b9d6897ed4f
surajgirioffl <surajkumargiri2002@gmail.com>
2023-03-07 13:48:40 +0530

    Add: Add schema for a new table `ip_blocklist`
    Add schema for a new table named as 'ip_blocklist'.
    
    Signed-off-by: @surajgirioffl

--- schema/block-list.sql
+++ schema/block-list.sql
@@ -16,3 +16,15 @@ CREATE TABLE IF NOT EXISTS `user_blocklist`(
     `UnblockedBy` VARCHAR(50) DEFAULT NULL
     /*BlockedBy & UnblockedBy - username or person/moderator/adminstrator who blocked the user*/
 );
+
+CREATE TABLE IF NOT EXISTS `ip_blocklist`(
+    `SNo` SMALLINT UNSIGNED NOT NULL UNIQUE AUTO_INCREMENT,
+    `BlockId` INT UNSIGNED PRIMARY KEY NOT NULL,
+    `Ip` VARCHAR(20) NOT NULL,
+    `BlockedOn` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `BlockedBy` VARCHAR(50) NOT NULL,
+    `BlockReason` VARCHAR(500) NOT NULL,
+    `UnblockedOn` DATETIME DEFAULT NULL,
+    `UnblockedBy` VARCHAR(50) DEFAULT NULL
+    /*BlockedBy & UnblockedBy - username or person/moderator/adminstrator who blocked the user*/
+);
