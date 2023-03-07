8e2e717db82760bb32605c8f2ea301392e1990c0
surajgirioffl <surajkumargiri2002@gmail.com>
2023-03-07 13:51:20 +0530

    Add: Add schema for a new table `blocked_ip_archive`
    Add schema for a new table named as 'blocked_ip_archive'.
    
    Signed-off-by: @surajgirioffl

--- schema/block-list.sql
+++ schema/block-list.sql
@@ -41,3 +41,16 @@ CREATE TABLE IF NOT EXISTS `blocked_users_archive`(
     FOREIGN KEY (`BlockId`) REFERENCES user_blocklist(BlockId)
     /*BlockedBy & UnblockedBy - username or person/moderator/adminstrator who blocked the user*/
 );
+
+CREATE TABLE IF NOT EXISTS `blocked_ip_archive`(
+    `SNo` SMALLINT UNSIGNED NOT NULL UNIQUE AUTO_INCREMENT,
+    `BlockId` INT UNSIGNED PRIMARY KEY NOT NULL,
+    `Ip` VARCHAR(20) NOT NULL,
+    `BlockedOn` DATETIME NOT NULL,
+    `BlockedBy` VARCHAR(50) NOT NULL,
+    `BlockReason` VARCHAR(500) NOT NULL,
+    `UnblockedOn` DATETIME DEFAULT CURRENT_TIMESTAMP,
+    `UnblockedBy` VARCHAR(50) NOT NULL,
+    FOREIGN KEY (`BlockId`) REFERENCES ip_blocklist(BlockId)
+    /*BlockedBy & UnblockedBy - username or person/moderator/adminstrator who blocked the user*/
+);
