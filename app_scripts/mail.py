e0524a3c03e7af941e6c2ddab458239f5b5e1260
surajgirioffl <surajkumargiri2002@gmail.com>
2023-03-08 16:13:08 +0530

    Docs: Add module docstring
    Add module docstring.
    
    Signed-off-by: @surajgirioffl

---
+++ app_scripts/mail.py
@@ -0,0 +1,12 @@
+"""
+    @file: mail.py
+    @author: Suraj Kumar Giri (@surajgirioffl)
+    @init-date: 8th March 2022
+    @last-modified: 8th March 2022
+    
+    @description:
+        Script to send mail (in single or in batch).
+"""
+
+__author__ = 'Suraj Kumar Giri'
+__email__ = 'surajgirioffl@gmail.com'
