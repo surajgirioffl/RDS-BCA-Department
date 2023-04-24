99385f7b5f9799f73298d38c09b114368164e8f1
surajgirioffl <surajkumargiri2002@gmail.com>
2023-04-24 18:11:16 +0530

    Add: Add a new script `previous_year_questions_db`
    Add a new script 'previous_year_questions_db' which will handle
    INSERT/UPDATE operations on the database 'previous_year_questions'.
    
    Signed-off-by: @surajgirioffl

---
+++ automation_scripts/previous_year_questions_db.py
@@ -0,0 +1,10 @@
+"""
+    @file: previous_year_questions_db.py
+    @author: Suraj Kumar Giri
+    @init-date: 24th April 2023
+    @last-modified: 24th April 2023
+    @error-series: 1900
+    
+    @description:
+        * Module to insert data into the database rdsbca$previous_year_questions.
+"""
