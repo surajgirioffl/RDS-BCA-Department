b3bb0a9fb59678ec3fcc6ba276494fdab6296554
surajgirioffl <surajkumargiri2002@gmail.com>
2023-04-29 19:07:16 +0530

    Add: Add a new script `files_db`
    Add a new script 'files_db' to handle all database related operations on
    the database rdsbca$files.
    
    Signed-off-by: @surajgirioffl

---
+++ db_scripts/files_db.py
@@ -0,0 +1,18 @@
+"""
+    @file: files_db.py
+    @author: Suraj Kumar Giri
+    @init-date: 29th April 2023
+    @last-modified: 29th April 2023
+    @error-series: 2100
+
+    @description:
+        * Module to fetch desired data from the database rdsbca$files.
+        * Module to read/insert/update desired data from/to the database rdsbca$files.
+        * Simply, module to handle all database related operations on the database rdsbca$files.
+    
+    @classes:
+        ...
+    
+    @functions:
+        ...
+"""
