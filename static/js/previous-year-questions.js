dd34ea6413f53f566ae6fd053ec0bd3fc5060b1e
surajgirioffl <surajkumargiri2002@gmail.com>
2023-05-16 05:28:39 +0530

    Fix: Fix issue with responseText trimming not working properly
    Fix issue with response text trimming not functioning properly
    because of XMLHttpRequest.responseText is read only.
    
    Signed-off-by: @surajgirioffl

--- static/js/previous-year-questions.js
+++ static/js/previous-year-questions.js
@@ -76,9 +76,13 @@ function displayPreviousYearQuestions() {
          * Removing the city name from the response text if screen width is less than 768px (mobile screen).
          * This is done because in mobile screen, if the name of source contains city name then height of table row will increase (in more than 1 line) and it will not look good because other data in same row are just one lined.
         */
-        if (window.screen.width <= 768)
-            request.responseText = request.responseText.replaceAll(', Muzaffarpur', '');
-        document.getElementById("previous-year-questions-container").innerHTML = request.responseText;
+        let responseText = request.responseText
+        if (window.screen.width <= 768) {
+            // request.responseText = request.responseText.replaceAll(', Muzaffarpur', '');
+            /* Above line will not work because request.responseText is read only*/
+            responseText = responseText.replaceAll(', Muzaffarpur', '');
+        }
+        document.getElementById("previous-year-questions-container").innerHTML = responseText;
         document.getElementById('loading-svg').style.display = 'none';
         if (request.status == 200)
             initializePopovers();
