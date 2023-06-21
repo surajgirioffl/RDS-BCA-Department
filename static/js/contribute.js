a558fb829e4fed24d7e3732a66b3f1914766c391
surajgirioffl <surajkumargiri2002@gmail.com>
2023-06-21 20:18:29 +0530

    Feat: Add feat to fetch contribution forms from server
    Add functionality to fetch contribution forms from the server.
    
    Signed-off-by: @surajgirioffl

---
+++ static/js/contribute.js
@@ -0,0 +1,48 @@
+
+/*fetch form from the server and display */
+const dropDown = document.getElementById("floatingSelect2");
+dropDown.addEventListener('change', () => {
+    if (dropDown.value != "") {
+        fetchFormAndDisplay(selectedOption = dropDown.value);
+    }
+    else {
+        document.getElementById("response-text-container").innerHTML = "";
+    }
+})
+
+const fetchFormAndDisplay = function (selectedOption) {
+
+    /*displaying loading animation/spinner while making the request and receiving the response from the server*/
+    const responseTextContainer = document.getElementById("response-text-container");
+    responseTextContainer.innerHTML = `
+                                        <div class="spinner-border text-primary" role="status">
+                                            <span class="visually-hidden">Loading...</span>
+                                        </div>
+                                        `;
+
+    const promise = new Promise((resolve, reject) => {
+        const request = new XMLHttpRequest();
+        if (window.location.hostname == '127.0.0.1')
+            request.open('POST', url = 'http://127.0.0.1:5000/api/fetch-contribution-form', true);
+        else
+            request.open('POST', url = 'https://rdsbca.pythonanywhere.com/api/fetch-contribution-form', true);
+
+        request.setRequestHeader('Content-Type', 'application/json')
+        request.send(JSON.stringify({ selectedOption: selectedOption }));
+        request.onload = () => {
+            if (request.status == 200)
+                resolve(request.responseText);
+            else
+                reject(request.responseText);
+        }
+    });
+
+    promise.then((responseText) => {
+        responseTextContainer.innerHTML = responseText;
+    })
+
+    promise.catch((error) => {
+        responseTextContainer.innerHTML = responseText;
+    })
+
+}
