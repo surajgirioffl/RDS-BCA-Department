447cbcb42b987541cb3dfb757e98a33eb8d133fa
Suraj Kumar Giri <surajkumargiri2002@gmail.com>
2024-02-16 09:18:36 +0530

    Add: Add module to handle operations on `admin` db
    Add module to handle database operations related with the database 'admin'.
    
    Signed-off-by: @surajgirioffl

---
+++ db_scripts2/admin_db.py
@@ -0,0 +1,9 @@
+"""
+    @file: db_scripts2/admin_db.py
+    @author: Suraj Kumar Giri (@surajgirioffl)
+    @init-date: 16th Feb 2024
+    @last-modified: 16th Feb 2024
+    
+    Description:
+        * Module to handle database operations related with the database 'admin'.
+"""
