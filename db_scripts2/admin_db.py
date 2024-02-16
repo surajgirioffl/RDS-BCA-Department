0b4ece702941b5f1e1c889ff803785ddfcb59798
Suraj Kumar Giri <surajkumargiri2002@gmail.com>
2024-02-16 09:19:43 +0530

    Feat: Add feat. to fetch admin details
    Add feat. to fetche the details of the admin with the given username
    from the admin database.
    
    Signed-off-by: @surajgirioffl

--- db_scripts2/admin_db.py
+++ db_scripts2/admin_db.py
@@ -7,3 +7,31 @@
     Description:
         * Module to handle database operations related with the database 'admin'.
 """
+
+from orm.admin_model import Admins
+from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy import create_engine
+
+
+def fetch_admin_details(username) -> dict | bool:
+    """
+    Fetches the details of the admin with the given username from the admin database.
+
+    Args:
+        username (str): The username of the admin.
+
+    Returns:
+        dict | bool: Returns a dictionary containing the admin details if the admin is found, otherwise returns False.
+    """
+    engine = create_engine("sqlite:///admin.db")
+    session: Session = sessionmaker(engine)()
+
+    # session.query(admin.Admins.password).one()
+    if admin_obj := session.query(Admins).filter(Admins.username == username).first():
+        data = {}
+        data["username"] = admin_obj.username
+        data["email"] = admin_obj.email
+        data["role"] = admin_obj.role
+        data["name"] = admin_obj.name
+        return data
+    return False
