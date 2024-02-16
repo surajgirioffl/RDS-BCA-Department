90e8903d707f491affb1e52452c243efd977c412
Suraj Kumar Giri <surajkumargiri2002@gmail.com>
2024-02-17 00:42:25 +0530

    Feat: Add feat. to check if admin logged in
    Add feat. to check if admin logged in.
    
    Signed-off-by: @surajgirioffl

--- auth.py
+++ auth.py
@@ -2,12 +2,13 @@
     @file: auth.py
     @author: Suraj Kumar Giri (@surajgirioffl)
     @init-date: 16th Feb 2024
-    @last-modified: 16th Feb 2024
+    @last-modified: 17th Feb 2024
     
     Description:
         Module to handle various authentication methods.
 """
 
+from flask import session
 from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy import create_engine
 from models import admin_model as admin
@@ -49,3 +50,16 @@ def is_admin(username: str) -> admin.Admins | None:
     session: Session = sessionmaker(engine)()
     # Implement OOPs later to avoid reparations of code.
     return session.query(admin.Admins).filter(admin.Admins.username == username).first()
+
+
+def is_admin_logged_in() -> bool:
+    """
+    Check if an admin is logged in by verifying the presence of "username" and "admin" in the session.
+    
+    Returns:
+        bool: True if the admin is logged in, otherwise returns False.
+    """
+    if "username" in session and "admin" in session:
+        if session.get("admin"):
+            return bool(is_admin(session.get("username")))
+    return False
