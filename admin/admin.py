b7b3d05462858f7f9cda90216a6b5abd4ff0cf06
Suraj Kumar Giri <surajkumargiri2002@gmail.com>
2024-02-16 10:58:03 +0530

    Add: Add module to handle admin panel
    Add main module to handle admin panel.
    
    Signed-off-by: @surajgirioffl

---
+++ admin/admin.py
@@ -0,0 +1,9 @@
+"""
+    @file: admin/admin.py
+    @author: Suraj Kumar Giri (@surajgirioffl)
+    @init-date: 11th Feb 2024
+    @last-modified: 15th Feb 2024
+    
+    Description:
+        * Main module to handle admin panel.
+"""
