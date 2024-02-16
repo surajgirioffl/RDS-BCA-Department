3f440cbdc21888529a499d15286769c8d102fc52
Suraj Kumar Giri <surajkumargiri2002@gmail.com>
2024-02-16 08:58:15 +0530

    Add: Add module handle various authentication methods
    Add module handle various authentication methods.
    Signed-off-by: @surajgirioffl

---
+++ auth.py
@@ -0,0 +1,9 @@
+"""
+    @file: auth.py
+    @author: Suraj Kumar Giri (@surajgirioffl)
+    @init-date: 16th Feb 2024
+    @last-modified: 16th Feb 2024
+    
+    Description:
+        Module to handle various authentication methods.
+"""
