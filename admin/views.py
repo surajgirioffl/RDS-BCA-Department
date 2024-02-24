00f651e7ad10246947149985bea5fefb29447135
Suraj Kumar Giri <surajkumargiri2002@gmail.com>
2024-02-25 04:08:30 +0530

    Feat: Add custom admin panel index view
    Add custom admin panel index view.
    
    Signed-off-by: @surajgirioffl

--- admin/views.py
+++ admin/views.py
@@ -7,3 +7,25 @@
     Description:
         * Main module to handle views of admin panel.
 """
+
+from flask_admin import AdminIndexView, expose
+from flask import session, redirect, url_for, flash
+import auth
+
+
+class MyIndexView(AdminIndexView):
+    """Custom admin panel index view class."""
+
+    @expose("/")
+    def index(self):
+        if "username" in session and auth.is_admin(session.get("username")):
+            # return self.render(self._template)
+            return self.render("admin/index.html")
+
+        if "username" in session:
+            # If user is logged-in but not an admin.
+            flash("You are not allowed to access the admin panel.", "warning")
+            return redirect(url_for("message"))
+        else:
+            flash("You are not logged-in. Please log-in to access admin panel.")
+            return redirect(url_for("login"))
