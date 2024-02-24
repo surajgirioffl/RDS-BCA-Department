ea34062059fab3ac97f282ed326bc730ed757c7f
Suraj Kumar Giri <surajkumargiri2002@gmail.com>
2024-02-25 04:20:31 +0530

    Feat: Implement OOPS to reduce the code redundency
    Implement OOPS to reduce the code redundency.
    
    Signed-off-by: @surajgirioffl

--- db_scripts2/admin_db.py
+++ db_scripts2/admin_db.py
@@ -2,7 +2,7 @@
     @file: db_scripts2/admin_db.py
     @author: Suraj Kumar Giri (@surajgirioffl)
     @init-date: 16th Feb 2024
-    @last-modified: 16th Feb 2024
+    @last-modified: 25th Feb 2024
     
     Description:
         * Module to handle database operations related with the database 'admin'.
@@ -13,48 +13,62 @@ from sqlalchemy.orm import sessionmaker, Session
 from models import admin_model
 
 
-def fetch_admin_details(username) -> dict | bool:
-    """
-    Fetches the details of the admin with the given username from the admin database.
+class AdminDatabase:
+    def __init__(self, engine_url="sqlite:///admin.db") -> None:
+        """Constructor for the class initializing the engine URL and session.
 
-    Args:
-        username (str): The username of the admin.
+        Parameters:
+            engine_url (str): The URL of the engine. Defaults to "sqlite:///admin.db".
 
-    Returns:
-        dict | bool: Returns a dictionary containing the admin details if the admin is found, otherwise returns False.
-    """
-    engine = create_engine("sqlite:///admin.db")
-    session: Session = sessionmaker(engine)()
+        Returns:
+            None
+        """
+        self.engine = create_engine(engine_url)
+        self.session: Session = sessionmaker(self.engine)()
 
-    # session.query(admin.Admins.password).one()
-    if admin_obj := session.query(admin_model.Admins).filter(admin_model.Admins.username == username).first():
-        data = {}
-        data["username"] = admin_obj.username
-        data["email"] = admin_obj.email
-        data["role"] = admin_obj.role
-        data["name"] = admin_obj.name
-        return data
-    return False
+    def __del__(self) -> None:
+        """This function is the destructor method for the class. It commits the current session."""
+        self.session.commit()
 
+    def fetch_admin_details(self, username) -> dict | bool:
+        """
+        Fetches the details of the admin with the given username from the admin database.
 
-def fetch_granted_permissions(username: str):
-    """Fetch granted permission of the given username.
+        Args:
+            username (str): The username of the admin.
 
-    Args:
-        username (str): username to fetch granted permission.
+        Returns:
+            dict | bool: Returns a dictionary containing the admin details if the admin is found, otherwise returns False.
+        """
 
-    Returns:
-        dict | False: Dictionary of granted permission if user exits and allowed to access admin panel else False.
-    """
-    engine = create_engine("sqlite:///admin.db")
-    session: Session = sessionmaker(engine)()
+        # session.query(admin.Admins.password).one()
+        if admin_obj := self.session.query(admin_model.Admins).filter(admin_model.Admins.username == username).first():
+            data = {}
+            data["username"] = admin_obj.username
+            data["email"] = admin_obj.email
+            data["role"] = admin_obj.role
+            data["name"] = admin_obj.name
+            return data
+        return False
 
-    if admin_details := fetch_admin_details(username):
-        if session.query(admin_model.AdminManager.is_allowed).filter(admin_model.AdminManager.username == username).first():
-            permissions = session.query(admin_model.Permissions.permission, getattr(admin_model.Permissions, admin_details["role"])).all()
-            permission_dict = {}
-            for row in permissions:
-                permission_dict[row[0]] = bool(row[1])
-            return permission_dict
+    def fetch_granted_permissions(self, username: str):
+        """Fetch granted permission of the given username.
 
-    return False
+        Args:
+            username (str): username to fetch granted permission.
+
+        Returns:
+            dict | False: Dictionary of granted permission if user exits and allowed to access admin panel else False.
+        """
+
+        if admin_details := self.fetch_admin_details(username):
+            if self.session.query(admin_model.AdminManager.is_allowed).filter(admin_model.AdminManager.username == username).first():
+                permissions = self.session.query(
+                    admin_model.Permissions.permission, getattr(admin_model.Permissions, admin_details["role"])
+                ).all()
+                permission_dict = {}
+                for row in permissions:
+                    permission_dict[row[0]] = bool(row[1])
+                return permission_dict
+
+        return False
