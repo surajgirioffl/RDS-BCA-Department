add841d7aa1a7afff02caade62851cee78e78f3b
Suraj Kumar Giri <surajkumargiri2002@gmail.com>
2024-02-25 00:48:45 +0530

    Add: Add module to handle views of admin panel
    Add module to handle views of admin panel.
    
    Signed-off-by: @surajgirioffl

---
+++ admin/views.py
@@ -0,0 +1,9 @@
+"""
+    @file: admin/views.py
+    @author: Suraj Kumar Giri (@surajgirioffl)
+    @init-date: 25th Feb 2024
+    @last-modified: 25th Feb 2024
+    
+    Description:
+        * Main module to handle views of admin panel.
+"""
