19b54221345ffe758546ea5d17c5ef8625951439
Suraj Kumar Giri <surajkumargiri2002@gmail.com>
2024-02-25 00:38:57 +0530

    Feat: Add feat. to fetch granted permission for given username
    Add functionality to fetch the granted permissions for the given
    username.
    
    Signed-off-by: @surajgirioffl

--- db_scripts2/admin_db.py
+++ db_scripts2/admin_db.py
@@ -8,9 +8,9 @@
         * Module to handle database operations related with the database 'admin'.
 """
 
-from models import admin_model
-from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
+from models import admin_model
 
 
 def fetch_admin_details(username) -> dict | bool:
@@ -34,4 +34,27 @@ def fetch_admin_details(username) -> dict | bool:
         data["role"] = admin_obj.role
         data["name"] = admin_obj.name
         return data
+    return False
+
+
+def fetch_granted_permissions(username: str):
+    """Fetch granted permission of the given username.
+
+    Args:
+        username (str): username to fetch granted permission.
+
+    Returns:
+        dict | False: Dictionary of granted permission if user exits and allowed to access admin panel else False.
+    """
+    engine = create_engine("sqlite:///admin.db")
+    session: Session = sessionmaker(engine)()
+
+    if admin_details := fetch_admin_details(username):
+        if session.query(admin_model.AdminManager.is_allowed).filter(admin_model.AdminManager.username == username).first():
+            permissions = session.query(admin_model.Permissions.permission, getattr(admin_model.Permissions, admin_details["role"])).all()
+            permission_dict = {}
+            for row in permissions:
+                permission_dict[row[0]] = bool(row[1])
+            return permission_dict
+
     return False
