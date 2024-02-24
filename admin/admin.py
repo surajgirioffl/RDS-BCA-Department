10612d0f5c4cae2ade8c2b169478dc81100f4363
Suraj Kumar Giri <surajkumargiri2002@gmail.com>
2024-02-25 00:46:31 +0530

    Feat: Add driver function to handle admin panel
    Add driver function to handle admin panel.
    
    Signed-off-by: @surajgirioffl

--- admin/admin.py
+++ admin/admin.py
@@ -2,8 +2,24 @@
     @file: admin/admin.py
     @author: Suraj Kumar Giri (@surajgirioffl)
     @init-date: 11th Feb 2024
-    @last-modified: 15th Feb 2024
+    @last-modified: 25th Feb 2024
     
     Description:
         * Main module to handle admin panel.
 """
+
+from flask import Flask
+from flask_admin import Admin
+
+
+def admin_panel(app: Flask) -> None:
+    """Driver function to handle admin panel.
+
+    Args:
+        app (Flask): Flask app.
+    """
+    app.config["FLASK_ADMIN_SWATCH"] = "cerulean"  # Setting bootswatch theme
+    admin = Admin(app, name="AdminPanel", template_mode="bootstrap4")
+
+    # administrative views
+    # ....
