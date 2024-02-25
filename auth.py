e70e156b528896d58a7580d4bacb0cd37f0ffa8c
Suraj Kumar Giri <surajkumargiri2002@gmail.com>
2024-02-25 20:11:14 +0530

    Update: Update method to check if user logged-in via session
    Update method to check if user logged-in via session only (No database).
    
    Also, add some more description in the docstring of the function.
    
    Signed-off-by: @surajgirioffl

--- auth.py
+++ auth.py
@@ -9,17 +9,19 @@
 """
 
 from flask import session
-from db_scripts2 import admin_db
 
 
 def is_admin_logged_in() -> bool:
-    """
-    Check if an admin is logged in by verifying the presence of "username" and "admin" in the session.
+    """Check if an admin is logged in by verifying the presence of "username" and "admin" in the session.
+
+    Warning - This method will check if admin logged-in using session only. There is no interaction with database.
+    It may possible that user is not allowed to access admin panel even if user is set as admin in the session.
+    So, Must check database before proceeding to next.
 
     Returns:
         bool: True if the admin is logged in, otherwise returns False.
     """
     if "username" in session and "admin" in session:
         if session.get("admin"):
-            return bool(admin_db.AdminDatabase().is_admin(session.get("username")))
+            return True
     return False
