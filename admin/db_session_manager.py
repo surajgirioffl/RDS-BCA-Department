6f9cacf86b5d15c84d8fd3efbcac0fa052f268b7
Suraj Kumar Giri <surajkumargiri2002@gmail.com>
2024-02-27 00:07:39 +0530

    Add: Add module to handle DBs session for admin panel
    Add module to handle DBs session for the admin panel.
    
    Signed-off-by: @surajgirioffl

---
+++ admin/db_session_manager.py
@@ -0,0 +1,9 @@
+"""
+    @file: admin/db_session_manager..py
+    @author: Suraj Kumar Giri (@surajgirioffl)
+    @init-date: 27th Feb 2024
+    @last-modified: 27th Feb 2024
+    
+    Description:
+        * Module to handle db session for the admin panel.
+"""
