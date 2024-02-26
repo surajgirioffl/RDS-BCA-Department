7c150ed8816d30b3c9f9ecef5f354cfa303c1d66
Suraj Kumar Giri <surajkumargiri2002@gmail.com>
2024-02-26 23:38:59 +0530

    Feat: Add a function to get message specific response
    Add a function to get message specific response (for reusability)
    and update the code accordingly.
    
    Signed-off-by: @surajgirioffl

--- admin/views.py
+++ admin/views.py
@@ -13,6 +13,34 @@ from flask import redirect, url_for, flash
 import auth
 
 
+def get_message_specific_response(message: str):
+    """A function to handle different types of messages and provide specific responses based on the message type.
+
+    Args:
+        message(str): A string representing the type of message.
+
+    Returns:
+        A redirection based on the message type.
+    """
+    if message == "restricted":
+        # User is admin but not allowed to access admin panel.
+        flash("Access Denied. You are not allowed to access the admin panel.", "warning")
+        flash("Hi Admin, You are restricted to access the admin panel. Please contact administrator.", "warning")
+        return redirect(url_for("message"))
+    elif message == "denied":
+        # User is logged-in but not an admin.
+        flash("Access Denied. You are not allowed to access the admin panel.", "warning")
+        return redirect(url_for("message"))
+    elif message == "anonymous":
+        # User is not logged-in.
+        flash("You are not logged-in. Please log-in to access the admin panel.")
+        return redirect(url_for("login"))
+    else:
+        # Something else.
+        flash("Access Denied.", "warning")
+        return redirect(url_for("message"))
+
+
 class MyIndexView(AdminIndexView):
     """Custom admin panel index view class."""
 
@@ -27,20 +55,4 @@ class MyIndexView(AdminIndexView):
             return self.render("admin/index.html")
 
         # Access denied.
-        if message == "restricted":
-            # User is admin but not allowed to access admin panel.
-            flash("Access Denied. You are not allowed to access the admin panel.", "warning")
-            flash("Hi Admin, You are restricted to access the admin panel. Please contact administrator.", "warning")
-            return redirect(url_for("message"))
-        elif message == "denied":
-            # User is logged-in but not an admin.
-            flash("Access Denied. You are not allowed to access the admin panel.", "warning")
-            return redirect(url_for("message"))
-        elif message == "anonymous":
-            # User is not logged-in.
-            flash("You are not logged-in. Please log-in to access the admin panel.")
-            return redirect(url_for("login"))
-        else:
-            # Something else.
-            flash("Access Denied.", "warning")
-            return redirect(url_for("message"))
+        return get_message_specific_response(message)
