e3fc2452af144badd12281d01700339e6f6f88d1
Suraj Kumar Giri <surajkumargiri2002@gmail.com>
2024-02-27 04:19:00 +0530

    Feat: Add feat. to get sqlite db session
    Add functionality to get sqlite database session.
    
    Signed-off-by: @surajgirioffl

--- admin/db_session_manager.py
+++ admin/db_session_manager.py
@@ -7,3 +7,20 @@
     Description:
         * Module to handle db session for the admin panel.
 """
+
+from os import environ
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session, sessionmaker
+
+
+def get_sqlite_db_session(database: str) -> Session:
+    """Function to get a SQLite database session.
+
+    Args:
+        database (str): The name of the database.
+
+    Returns:
+        Session: A session object connected to the specified SQLite database.
+    """
+    engine = create_engine(f"sqlite:///{database}.db")
+    return sessionmaker(bind=engine)()
