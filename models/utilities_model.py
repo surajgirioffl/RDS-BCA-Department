f8103109018cfc849353cfb64f53314f36c6f2db
Suraj Kumar Giri <surajkumargiri2002@gmail.com>
2024-02-27 17:58:00 +0530

    Add: Add the module to store models of the `utilities` database
    Add the module to store models of the 'utilities' database.
    
    Signed-off-by: @surajgirioffl

---
+++ models/utilities_model.py
@@ -0,0 +1,9 @@
+"""
+    @file: orm/utilities_model.py
+    @author: Suraj Kumar Giri (@surajgirioffl)
+    @init-date: 27th Feb 2024
+    @last-modified: 27th Feb 2024
+    
+    Description:
+        * Module containing Models associated with the database 'utilities'.
+"""
