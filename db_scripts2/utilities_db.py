58560fc83437194ff3fbc4fa98aabe0120909c32
Suraj Kumar Giri <surajkumargiri2002@gmail.com>
2024-02-27 18:00:43 +0530

    Add: Add module to handle database operations of `utilities` db
    Add model to handle database operations related with the database
    'utilities'.
    
    Signed-off-by: @surajgirioffl

---
+++ db_scripts2/utilities_db.py
@@ -0,0 +1,9 @@
+"""
+    @file: db_scripts2/utilities_db.py
+    @author: Suraj Kumar Giri (@surajgirioffl)
+    @init-date: 27th Feb 2024
+    @last-modified: 27th Feb 2024
+    
+    Description:
+        * Module to handle database operations related with the database 'utilities'.
+"""
