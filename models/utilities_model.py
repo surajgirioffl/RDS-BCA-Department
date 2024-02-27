24a1dfdcffe0e4da71ab50fde52c776f155a74db
Suraj Kumar Giri <surajkumargiri2002@gmail.com>
2024-02-27 17:59:16 +0530

    Add: Add some models related to OTP
    Add some models related to OTP.
    
    Signed-off-by: @surajgirioffl

--- models/utilities_model.py
+++ models/utilities_model.py
@@ -7,3 +7,33 @@
     Description:
         * Module containing Models associated with the database 'utilities'.
 """
+
+from sqlalchemy import Integer, String, Enum, DateTime, create_engine
+from sqlalchemy.orm import mapped_column, DeclarativeBase, sessionmaker
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Otp(Base):
+    __tablename__ = "otp"
+    id = mapped_column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    username = mapped_column(String, nullable=False)
+    otp = mapped_column(String, nullable=False)
+    creation_time = mapped_column(DateTime, nullable=False, server_default="CURRENT_TIMESTAMP")
+    expiration_time = mapped_column(DateTime, nullable=False)
+
+
+class OtpLog(Base):
+    __tablename__ = "otp_log"
+    id = mapped_column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    username = mapped_column(String, nullable=False)
+    otp = mapped_column(String, nullable=False)
+    status = mapped_column(Enum("generated", "verified", "expired"), nullable=False)
+    creation_time = mapped_column(DateTime, nullable=False)
+    logged_time = mapped_column(DateTime, nullable=False, server_default="CURRENT_TIMESTAMP")
+
+
+engine = create_engine("sqlite:///utilities.db")
+Base.metadata.create_all(engine)
