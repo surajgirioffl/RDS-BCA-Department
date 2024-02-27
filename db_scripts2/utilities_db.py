7dbed885d705988ce2aa8f850a91cf903c2b4fe7
Suraj Kumar Giri <surajkumargiri2002@gmail.com>
2024-02-27 18:08:25 +0530

    Feat: Add constructor & destructor of the `UtilitiesDB` class
    Add constructor and destructor of the 'UtilitiesDB' class.
    
    Signed-off-by: @surajgirioffl

--- db_scripts2/utilities_db.py
+++ db_scripts2/utilities_db.py
@@ -7,3 +7,26 @@
     Description:
         * Module to handle database operations related with the database 'utilities'.
 """
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
+from models import utilities_model
+
+
+class UtilitiesDB:
+    def __init__(self, engine_url="sqlite:///admin.db") -> None:
+        """Constructor for the class initializing the engine URL and session.
+
+        Parameters:
+            engine_url (str): The URL of the engine. Defaults to "sqlite:///admin.db".
+
+        Returns:
+            None
+        """
+        self.engine = create_engine(engine_url)
+        self.session: Session = sessionmaker(self.engine)()
+
+    def __del__(self) -> None:
+        """This function is the destructor method for the class. It commits the current session."""
+        self.session.commit()
+        self.session.close()
