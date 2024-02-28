19ef1f4fe52e727aee7829b23cf93975b62a2ac0
Suraj Kumar Giri <surajkumargiri2002@gmail.com>
2024-02-28 13:00:51 +0530

    Fix: Fix `Invalid isoformat string: 'CURRENT_TIMESTAMP'`
    Fix the issue of Invalid isoformat string: 'CURRENT_TIMESTAMP' by adding
    Python datetime as default value of the timestamp.
    
    Signed-off-by: @surajgirioffl

--- models/utilities_model.py
+++ models/utilities_model.py
@@ -2,12 +2,13 @@
     @file: orm/utilities_model.py
     @author: Suraj Kumar Giri (@surajgirioffl)
     @init-date: 27th Feb 2024
-    @last-modified: 27th Feb 2024
+    @last-modified: 28th Feb 2024
     
     Description:
         * Module containing Models associated with the database 'utilities'.
 """
 
+from datetime import datetime
 from sqlalchemy import Integer, String, Enum, DateTime, create_engine
 from sqlalchemy.orm import mapped_column, DeclarativeBase, sessionmaker
 
@@ -21,7 +22,7 @@ class Otp(Base):
     id = mapped_column(Integer, primary_key=True, autoincrement=True, nullable=False)
     username = mapped_column(String, nullable=False)
     otp = mapped_column(String, nullable=False)
-    creation_time = mapped_column(DateTime, nullable=False, server_default="CURRENT_TIMESTAMP")
+    creation_time = mapped_column(DateTime, nullable=False, default=datetime.now())
     expiration_time = mapped_column(DateTime, nullable=False)
 
 
@@ -32,7 +33,7 @@ class OtpLog(Base):
     otp = mapped_column(String, nullable=False)
     status = mapped_column(Enum("generated", "verified", "expired"), nullable=False)
     creation_time = mapped_column(DateTime, nullable=False)
-    logged_time = mapped_column(DateTime, nullable=False, server_default="CURRENT_TIMESTAMP")
+    logged_time = mapped_column(DateTime, nullable=False, default=datetime.now())
 
 
 engine = create_engine("sqlite:///utilities.db")
