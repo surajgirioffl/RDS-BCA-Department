cca3e691f1f9e4adc739de7b1850018f849de358
Suraj Kumar Giri <surajkumargiri2002@gmail.com>
2024-03-04 10:35:23 +0530

    Feat: Add timezone in the current timestamp
    Add timezone in the current timestamp.
    
    Signed-off-by: @surajgirioffl

--- models/utilities_model.py
+++ models/utilities_model.py
@@ -2,15 +2,18 @@
     @file: orm/utilities_model.py
     @author: Suraj Kumar Giri (@surajgirioffl)
     @init-date: 27th Feb 2024
-    @last-modified: 28th Feb 2024
+    @last-modified: 4th March 2024
     
     Description:
         * Module containing Models associated with the database 'utilities'.
 """
 
 from datetime import datetime
+import pytz
 from sqlalchemy import Integer, String, Enum, DateTime, create_engine
 from sqlalchemy.orm import mapped_column, DeclarativeBase, sessionmaker
+
+tz = pytz.timezone("Asia/Kolkata")
 
 
 class Base(DeclarativeBase):
@@ -22,7 +25,7 @@ class Otp(Base):
     id = mapped_column(Integer, primary_key=True, autoincrement=True, nullable=False)
     username = mapped_column(String, nullable=False)
     otp = mapped_column(String, nullable=False)
-    creation_time = mapped_column(DateTime, nullable=False, default=datetime.now())
+    creation_time = mapped_column(DateTime, nullable=False, default=datetime.now(tz=tz))
     expiration_time = mapped_column(DateTime, nullable=False)
 
 
@@ -33,7 +36,7 @@ class OtpLog(Base):
     otp = mapped_column(String, nullable=False)
     status = mapped_column(Enum("generated", "verified", "expired"), nullable=False)
     creation_time = mapped_column(DateTime, nullable=False)
-    logged_time = mapped_column(DateTime, nullable=False, default=datetime.now())
+    logged_time = mapped_column(DateTime, nullable=False, default=datetime.now(tz=tz))
 
 
 engine = create_engine("sqlite:///utilities.db")
